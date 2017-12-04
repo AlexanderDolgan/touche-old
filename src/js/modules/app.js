bc8a936ea1f4e8bb22fe2ec1240cfedb3c66520c
AlexanderDolgan <5704737@gmail.com>
2017-12-04 17:47:43 +0300

    ::bug

--- src/js/modules/app.js
+++ src/js/modules/app.js
@@ -1,11 +1,25 @@
 $(document).ready(function(){
 	
-	$('.ham-btn').on('click', function(evt) {
+	$('.ham-btn').on('click', function() {
 		$('.nav-wrapper').css('left', '0');
 	});
 
 
-	$('.close-btn').on('click', function(evt) {
+	$('.close-btn').on('click', function() {
+		$('.nav-wrapper').css('left', '-100%');
+	});
+
+
+	$('.centerer').on('click', function() {
+		$('.about').fadeOut();
+	});
+
+	$('.close-about').on('click', function() {
+		$('.about').fadeOut();
+	});
+
+	$('.about-btn').on('click', function() {
+		$('.about').fadeIn();
 		$('.nav-wrapper').css('left', '-100%');
 	});
 
