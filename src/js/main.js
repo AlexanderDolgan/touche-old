71f3a5c9331fc02ee9a47576adc510a921ec0028
AlexanderDolgan <5704737@gmail.com>
2017-12-05 15:37:29 +0300

    photoswipe

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,5 @@
 //@codekit-prepend "../../bower_components/jquery/dist/jquery.js"
 
 //@codekit-append "modules/app.js"
+//@codekit-append "../../bower_components/photoswipe/dist/photoswipe.min.js"
+//@codekit-append "../../bower_components/photoswipe/dist/photoswipe-ui-default.min.js"
--- src/js/modules/app.js
+++ src/js/modules/app.js
@@ -2,13 +2,13 @@ $(document).ready(function(){
 	
 	$('.ham-btn').on('click', function() {
 		$('.nav-wrapper').css('left', '0');
+		$('.main-nav').addClass('active');
 	});
-
 
 	$('.close-btn').on('click', function() {
 		$('.nav-wrapper').css('left', '-100%');
+		$('.main-nav').removeClass('active');
 	});
-
 
 	$('.centerer').on('click', function() {
 		$('.about').slideUp();
