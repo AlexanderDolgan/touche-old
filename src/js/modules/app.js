1cb20481efeea8c5c60a4b83826926e33542a602
AlexanderDolgan <5704737@gmail.com>
2017-12-05 21:45:13 +0300

    dist

--- src/js/modules/app.js
+++ src/js/modules/app.js
@@ -23,4 +23,27 @@ $(document).ready(function(){
 		$('.nav-wrapper').css('left', '-100%');
 	});
 
+	$('.image-gallery').slick({
+		infinite: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: false,
+		asNavFor: '.image-gallery-nav',
+		responsive: [
+			{
+				breakpoint: 480,
+				settings: "unslick"
+			}
+		]
+	});
+	$('.image-gallery-nav').slick({
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		asNavFor: '.image-gallery',
+		centerMode: true,
+		focusOnSelect: true
+	});
+
+	
+
 });
